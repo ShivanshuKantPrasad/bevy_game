f451813e0371831fc0df5786fa6bdb558d802961
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2022-05-31 19:49:28 +0530

    Controlling our Snake

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,28 @@ struct SnakeHead;
 
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
 
-fn main() {
-    App::new()
-        .add_system(setup_camera)
-        .add_system(spawn_snake)
-        .add_plugins(DefaultPlugins)
-        .add_system(exit_on_esc_system)
-        .run();
-}
-
 fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn snake_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+) {
+    for mut transform in head_positions.iter_mut(){
+         if keyboard_input.pressed(KeyCode::Left) {
+            transform.translation.x -= 2.;
+        }
+         if keyboard_input.pressed(KeyCode::Right) {
+            transform.translation.x += 2.;
+        }
+         if keyboard_input.pressed(KeyCode::Down) {
+            transform.translation.y -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Up) {
+            transform.translation.y += 2.;
+        }
+    }
 }
 
 fn spawn_snake(mut commands: Commands) {
@@ -33,4 +44,14 @@ fn spawn_snake(mut commands: Commands) {
             ..default()
         })
         .insert(SnakeHead);
+}
+
+fn main() {
+    App::new()
+        .add_startup_system(setup_camera)
+        .add_startup_system(spawn_snake)
+        .add_system(snake_movement)
+        .add_plugins(DefaultPlugins)
+        .add_system(exit_on_esc_system)
+        .run();
 }
